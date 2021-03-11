c18c3c98ba9ea10b71b5953398e2e5edcf9d4200
Андрій Колесник <andriykolesnuk2000@gmail.com>
2021-03-11 16:48:55 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'User screens',
+      title: 'User screens!',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
